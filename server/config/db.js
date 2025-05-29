dd8e801e2e2188393b76f3597da5b349ddf0b50d
jenish298 <jenishbhadani9@gmail.com>
2025-05-29 15:37:38 +0530

    dvs

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 // function to connect to the mongodb database
 
-const connectDB = async() =>{
+const connectDB = async() =>{ 
     mongoose.connection.on('connected',()=> console.log('databse connected'))
 
     await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
